c81dcd25b4bcbf7a8330a1f91ef37e7d60baed23
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2021-03-12 14:56:32 -0300

    commit lifecycle
    entendendo o processo do ciclo de vida dos aplicativos android

--- app/src/main/java/com/dantasbarreto/imcaplication/MainActivity.kt
+++ app/src/main/java/com/dantasbarreto/imcaplication/MainActivity.kt
@@ -2,10 +2,43 @@ package com.dantasbarreto.imcaplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("lifeclycle", "entrei no CREATE - estou criando a tela")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle", "entrei no START - deixei a tela visível para você")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle", "entrei no RESUME - agora você pode interagir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle", "entrei no PAUSE - a tela perdeu o foco, você não pode mais ineragir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifecycle", "entrei no STOP - a tela não está mais visível mas ainda existe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifecycle", "entrei no RESTART - a tela está retomando o foco")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifecycle", "entrei no DESTROY - ah não! a tela foi destruída")
     }
 }
